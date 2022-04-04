6cf7edbec908154adf52634c280e902e8cbc8571
Kamil Galazka <kamilg332@gmail.com>
2022-04-05 00:44:12 +0200

    modified index.html style.css script.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,33 @@
-//zmiana opacity na headerze
-window.addEventListener('scroll', function() {
-    const scrollFromTop = window.scrollY;
-    const headerChange = document.querySelector('header');
-    const headerHeight = document.querySelector('.about').offsetTop;
-    
-    headerChange.style.opacity = 1 - scrollFromTop/headerHeight;
-})
+let lightTheme = false;
 
-//pojawienie się skillsContainer
-$('button.showSkills').on('click', function(){
-    $('button.showSkills').attr('id', 'inactive');
-    $('div.skillsContainer').removeAttr('id', 'inactive')
-})
+//change opacity on header
+window.addEventListener("scroll", function () {
+  const scrollFromTop = window.scrollY;
+  const headerChange = document.querySelector("header");
+  const headerHeight = document.querySelector(".about").offsetTop;
 
-//przeładowanie strony
-document.querySelector('.reload').addEventListener('click', function(){
-    console.log('dsas');
-    window.location.reload();
-})
+  headerChange.style.opacity = 1 - scrollFromTop / headerHeight;
+});
+
+//change class in skills-container - jQuery
+$("button.show-skills").on("click", function () {
+  $("button.show-skills").attr("id", "inactive");
+  $("div.skills-container").removeAttr("id", "inactive");
+});
+
+//page reload
+document.querySelector(".reload").addEventListener("click", function () {
+  window.location.reload();
+});
+
+//dark or light mode
+document.querySelector("span.slider").addEventListener("click", function () {
+    const divContainer = document.querySelector("div.container");
+  if (lightTheme === false) {
+    divContainer.setAttribute("data-theme", "light");
+  } else {
+    divContainer.setAttribute("data-theme", "");
+  }
+
+  lightTheme = !lightTheme;
+});
